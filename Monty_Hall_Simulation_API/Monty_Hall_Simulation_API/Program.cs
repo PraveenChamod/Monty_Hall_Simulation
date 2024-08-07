a4f4670239b2cc2eb18d6df11c1fd96dfd95133b
Praveen Chamod <praveen.c@fidenz.com>
2024-08-08 01:39:24 +0530

    Add dependency injections.

--- Monty_Hall_Simulation_API/Monty_Hall_Simulation_API/Program.cs
+++ Monty_Hall_Simulation_API/Monty_Hall_Simulation_API/Program.cs
@@ -1,15 +1,18 @@
+using Monty_Hall_Simulation_API.Interfaces;
+using Monty_Hall_Simulation_API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<Random>();
+builder.Services.AddTransient<IGame, Game>();
+builder.Services.AddTransient<ISimulation, Simulation>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
