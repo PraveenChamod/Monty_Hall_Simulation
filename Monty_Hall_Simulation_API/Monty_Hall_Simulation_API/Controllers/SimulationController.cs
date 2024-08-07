6415b048a017ade2d791e3b02ffb632a97cfeab3
Praveen Chamod <praveen.c@fidenz.com>
2024-08-08 01:39:41 +0530

    Create simulation controller.

---
+++ Monty_Hall_Simulation_API/Monty_Hall_Simulation_API/Controllers/SimulationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Monty_Hall_Simulation_API.Interfaces;
+using Monty_Hall_Simulation_API.Models;
+
+namespace Monty_Hall_Simulation_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimulationController : ControllerBase
+    {
+        private readonly ISimulation _simulationService;
+
+        public SimulationController(ISimulation simulationService)
+        {
+            _simulationService = simulationService;
+        }
+
+        [HttpPost("Start")]
+        public IActionResult StartSimulations([FromQuery] int simulationCount, [FromQuery] bool switchStatus)
+        {
+            if (simulationCount <= 0)
+            {
+                return BadRequest("Simulation count must be greater than zero.");
+            }
+
+            try
+            {
+                Result result = _simulationService.StartSimulations(simulationCount, switchStatus);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+    }
+}
