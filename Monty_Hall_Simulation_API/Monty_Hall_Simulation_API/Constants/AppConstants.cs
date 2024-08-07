a87f756bdbb7ec458a0439a44ac441664c1bef01
Praveen Chamod <praveen.c@fidenz.com>
2024-08-08 01:40:03 +0530

    Define app constants.

---
+++ Monty_Hall_Simulation_API/Monty_Hall_Simulation_API/Constants/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Monty_Hall_Simulation_API.Constants
+{
+    public class AppConstants
+    {
+        // Exception Texts
+        public readonly static string SIMULATION_COUNT_ERROR_MESSAGE = "Simulation count must be greater than zero!";
+        public readonly static string DOOR_ALREADY_OPEN_ERROR_MESSAGE = "Door is already opened by the speaker!";
+
+        // Game Items
+        public readonly static string CAR = "Car";
+        public readonly static string GOAT = "Goat";
+    }
+}
