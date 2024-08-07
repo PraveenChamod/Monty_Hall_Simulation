121b19ece5f6c54ae96788f1a026c49e2c66d5e1
Praveen Chamod <praveen.c@fidenz.com>
2024-08-08 01:38:32 +0530

    Create the interfaces.

---
+++ Monty_Hall_Simulation_API/Monty_Hall_Simulation_API/Interfaces/IGame.cs
@@ -0,0 +1,18 @@
+﻿using Monty_Hall_Simulation_API.Enums;
+using Monty_Hall_Simulation_API.Models;
+
+namespace Monty_Hall_Simulation_API.Interfaces
+{
+    public interface IGame
+    {
+        Door UserChoosesDoor(int doorIndex);
+
+        Door UserChoosesDoor(DoorState doorState);
+
+        int IndexOfDoor(Door door);
+
+        Door SpeakerOpensDoor();
+
+        void ResetGame();
+    }
+}
---
+++ Monty_Hall_Simulation_API/Monty_Hall_Simulation_API/Interfaces/ISimulation.cs
@@ -0,0 +1,9 @@
+﻿using Monty_Hall_Simulation_API.Models;
+
+namespace Monty_Hall_Simulation_API.Interfaces
+{
+    public interface ISimulation
+    {
+        Result StartSimulations(int simulationCount, bool switchStatus);
+    }
+}
