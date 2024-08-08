9f60003f0203254e4454da130fb87c01c6799a0c
Praveen Chamod <praveen.c@fidenz.com>
2024-08-08 16:16:57 +0530

    Implement the configurations for simulation component.

---
+++ Monty_Hall_Simulation_Frontend/src/app/components/simulation/simulation.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SimulationService } from '../../services/simulation.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-simulation',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './simulation.component.html',
+  styleUrl: './simulation.component.scss',
+})
+export class SimulationComponent {
+  simulationCount = 0;
+  switchStatus = true;
+  gameCount = 0;
+  winCount = 0;
+  winRate = 0.00;
+  winRatePercentage = 0;
+  isLoading = false;
+
+  constructor(private simulationService: SimulationService) {}
+
+  onStart(): void {
+    this.isLoading = true;
+    this.simulationService
+      .startSimulation(this.simulationCount, this.switchStatus)
+      .subscribe({
+        next: (response) => {
+          this.gameCount = response.gameCount;
+          this.winCount = response.winCount;
+          this.winRate = response.winRate;
+          this.winRatePercentage = response.winRatePercentage;
+        },
+        error: (error) => {
+          console.error('Error fetching simulation data', error);
+        },
+        complete: () => {
+          this.isLoading = false;
+        },
+      });
+  }
+}
---
+++ Monty_Hall_Simulation_Frontend/src/app/services/simulation.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SimulationService {
+  private apiUrl = environment.API_URL;
+
+  constructor(private http: HttpClient) {}
+
+  startSimulation(
+    simulationCount: number,
+    switchStatus: boolean
+  ): Observable<any> {
+    const url = `${this.apiUrl}?simulationCount=${simulationCount}&switchStatus=${switchStatus}`;
+    return this.http.post<any>(url, {});
+  }
+}
